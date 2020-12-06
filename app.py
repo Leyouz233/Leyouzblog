b789bf9da37d4741f8ecc46be55beb3ac7bf2458
f-boss <2362315840@qq.com>
2020-12-06 12:33:07 +0800

    完成基础网页

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+app.config.from_pyfile('settings.py')
+pages = FlatPages(app)
+freezer = Freezer(app)
+
+
+@app.route('/')
+def home():
+    posts = [page for page in pages if 'date' in page.meta]
+    # Sort pages by date
+    sorted_posts = sorted(posts, reverse=True,
+                          key=lambda page: page.meta['date'])
+    return render_template('index.html', pages=sorted_posts)
+
+
+@app.route('/article/<path:path>/')
+def page(path):
+    page = pages.get_or_404(path)
+    return render_template('page.html', page=page)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ freeze.py
@@ -0,0 +1,5 @@
+from flask_frozen import Freezer
+from app import freezer
+
+if __name__ == '__main__':
+    freezer.freeze()
