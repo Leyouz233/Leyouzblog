49c95955b5264d00c69333b1c12353c48e22a701
Fakcer <2362315840@qq.com>
2020-12-09 15:30:05 +0800

    去除没用的冻结模块

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
 from flask_flatpages import FlatPages
-from flask_frozen import Freezer
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 pages = FlatPages(app)
-freezer = Freezer(app)
 
 
 @app.route('/')
--- freeze.py
+++
@@ -1,5 +0,0 @@
-from flask_frozen import Freezer
-from app import freezer
-
-if __name__ == '__main__':
-    freezer.freeze()
